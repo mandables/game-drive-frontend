b3145af13de080d20e03092bfbbb001c4ec4763b
amaan19 <amaan19@hotmail.com>
2019-03-19 14:26:44 +0000

    added checkboxes for played and completed

--- src/Display Components/GameInfo.js
+++ src/Display Components/GameInfo.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './GameInfo.css';
 
 class GameInfo extends Component {
+
+    handleClick = e => {
+        debugger
+    }
     render() {
         return (
             <div className="main-info">
                 <img src={this.props.game.image} />
                 <br />
                 {this.props.game.title}
+                <br />
+                <label >Played?</label>
+                <input onChange={this.handleClick} type="checkbox" name="played" />
+                <label>Completed?</label>
+                <input type="checkbox" name="completed" />
 
             </div>
         );
