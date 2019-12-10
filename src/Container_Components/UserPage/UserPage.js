417869b54f58ad3bbc624388628827f924f3ba17
Mani <amaan.ahmed3@gmail.com>
2019-12-10 22:04:46 +0000

    Fixed up user page

--- src/Container_Components/UserPage/UserPage.js
+++ src/Container_Components/UserPage/UserPage.js
@@ -1,53 +1,42 @@
-import React, { Component } from 'react';
-import './UserPage.css'
-import GameCard from '../../Display_Components/GameCard/GameCard'
-import UserGameCollection from '../../Display_Components/UserGameCollection/UserGameCollection'
-import Sidebar from '../../Display_Components/Sidebar/Sidebar'
+import React, { Component } from "react";
+import "./UserPage.css";
+import GameCard from "../../Display_Components/GameCard/GameCard";
+import UserGameCollection from "../../Display_Components/UserGameCollection/UserGameCollection";
+import Sidebar from "../../Display_Components/Sidebar/Sidebar";
 
-const url = 'http://localhost:3001/api/v1/users'
-
+const url = "http://localhost:3001/api/v1/users";
 
 class UserPage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            userGames: []
-        }
-    }
+  state = {
+    userGames: []
+  };
 
-    //fetch user's games
-    fetchUserGames = () => {
+  //fetch user's games
+  fetchUserGames = () => {
+    let id = parseInt(this.props.match.params.gamerId);
+    fetch(`${url}/${id}`)
+      .then(resp => resp.json())
+      .then(array => this.setState({ userGames: array.games }));
+  };
 
-        let id = parseInt(this.props.match.params.gamerId)
-        fetch(`${url}/${id}`)
-            .then(resp => resp.json())
-            .then(array => this.setState({ userGames: array.games }))
-    }
+  // componentDidMount() {
+  //     this.fetchUserGames()
+  // }
 
-    // componentDidMount() {
-    //     this.fetchUserGames()
-    // }
+  userGames = () => {
+    this.fetchUserGames();
+    return this.state.userGames.map(game => <UserGameCollection game={game} />);
+  };
 
-
-    userGames = () => {
-        this.fetchUserGames()
-        return this.state.userGames.map(game => <UserGameCollection game={game} />)
-    }
-
-    render() {
-
-        return (
-            <div className='user-main'>
-                <h1>{this.props.user.username}</h1>
-                <hr className="user-line" />
-                <div className="user-games">
-
-                    {this.userGames()}
-                </div >
-            </div>
-
-        );
-    }
+  render() {
+    return (
+      <div className="user-main">
+        <h1>{this.props.user.username}</h1>
+        <hr className="user-line" />
+        <div className="user-games">{this.userGames }</div>
+      </div>
+    );
+  }
 }
 
 export default UserPage;
